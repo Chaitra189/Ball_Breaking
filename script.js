2f042cd8cf292ac276d8e88e6c7b078dc67ba959
Chaitra189 <155656745+Chaitra189@users.noreply.github.com>
2024-11-10 11:24:28 +0530

    Added animations

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+document.querySelector('.animated-figure').addEventListener('click', function() {
+    const bubble = document.querySelector('.thought-bubble');
+    bubble.classList.toggle('visible'); // Toggle visibility of the thought bubble
+});
+
+
+
 
 
